d8b1d84b901ee2db3800e8971f95acec69217c23
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2022-10-20 23:21:35 +0200

    get all blends and post blend works

--- blends/src/main/java/com/example/blends/BlendsController.java
+++ blends/src/main/java/com/example/blends/BlendsController.java
@@ -1,13 +1,46 @@
 package com.example.blends;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.blends.model.Blend;
+import com.example.blends.repository.BlendsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
+@RequestMapping("/blendsapi")
 public class BlendsController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Hello!";
+    @Autowired
+    BlendsRepository blendsRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Blend>> getAllBlends() {
+        try {
+
+            List<Blend> blends = new ArrayList<Blend>(blendsRepository.findAll());
+
+            if (blends.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(blends, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Blend> createBlend(Blend blend) {
+        try {
+            Blend _bend = blendsRepository.save(new Blend(blend.getName(), blend.getId()));
+            return new ResponseEntity<>(_bend, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- blends/src/main/java/com/example/blends/model/Blend.java
+++ blends/src/main/java/com/example/blends/model/Blend.java
@@ -7,8 +7,28 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Blend {
     @Id
     private String id;
-
     private String name;
+
+    public Blend(String name, String id) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Override
     public String toString() {
