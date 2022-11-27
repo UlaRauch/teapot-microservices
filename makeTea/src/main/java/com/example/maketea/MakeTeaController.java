1d66103f1009dc21aed8694ea3cb7cf98dad7cc4
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2022-11-27 01:25:53 +0100

    (WIP) makeTea gets string for tea-id, returns cup of tea without availability check

--- makeTea/src/main/java/com/example/maketea/MakeTeaController.java
+++ makeTea/src/main/java/com/example/maketea/MakeTeaController.java
@@ -1,9 +1,13 @@
 package com.example.maketea;
 
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
@@ -13,14 +17,15 @@ import java.util.Optional;
 public class MakeTeaController {
 
     @GetMapping
-    public ResponseEntity<String> makeTea(@RequestBody String nameOfTea) {
-
+    public ResponseEntity<String> makeTea(@RequestBody String blendId) {
+    //public ResponseEntity<String> makeTea(@RequestBody String nameOfTea) {
         try {
             //TODO: check for empty name doesn't work :/
-            if (nameOfTea.isEmpty() || nameOfTea.isBlank()) {
+            if (blendId.isEmpty() || blendId.isBlank()) {
                 return new ResponseEntity<>("Boring hot water.", HttpStatus.OK);
             } else {
                 //TODO: call blends service and ask if blend is actually available
+                String nameOfTea = "Tea Placeholder";
                 return new ResponseEntity<>("Here is your cup of " + nameOfTea + ". Enjoy!", HttpStatus.OK);
             }
         } catch (Exception e) {
